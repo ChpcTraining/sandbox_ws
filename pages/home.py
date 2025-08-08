23b9e1c36a83f18bdb18843a5edbf13669babde3
ChpcTraining <157092105+ChpcTraining@users.noreply.github.com>
2025-08-08 09:40:02 +0200

    Create home.py

---
+++ pages/home.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+import streamlit as st
+from pathlib import Path
+from io import StringIO
+
+st.set_page_config(page_title="Home Viewer", layout="wide")
+st.title("📂 Home: Assignment 2")
+
+st.write("For more information, contact us on Slack")
